d26ebf80b293c2b424a7cd1f6b4a627623af8e85
Jongyoul Lee <jongyoul@gmail.com>
2013-09-26 17:36:38 +0900

    WordCountMapOnly 추가

---
+++ src/main/java/wikibooks/hadoop/chapter04/WordCountMapOnly.java
@@ -0,0 +1,45 @@
+package wikibooks.hadoop.chapter04;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jl
+ * Date: 9/26/13
+ * Time: 5:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class WordCountMapOnly {
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        if (args.length != 2) {
+            System.err.println("Usage: WordCount <input> <output>");
+            System.exit(2);
+        }
+        Job job = new Job(conf, "WordCount");
+
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(WordCountMapper.class);
+//        job.setReducerClass(WordCountReducer.class);
+        job.setNumReduceTasks(0);
+
+        job.setInputFormatClass(TextInputFormat.class);
+        job.setOutputFormatClass(TextOutputFormat.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.waitForCompletion(true);
+    }
+}
