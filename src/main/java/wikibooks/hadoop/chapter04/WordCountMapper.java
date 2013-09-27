40a4fcbfdda235f2393025379576ffe2a9646ce9
Jongyoul Lee <jongyoul@gmail.com>
2013-09-27 09:03:51 +0900

    streaming 스크립트 추가

--- src/main/java/wikibooks/hadoop/chapter04/WordCountMapper.java
+++ src/main/java/wikibooks/hadoop/chapter04/WordCountMapper.java
@@ -1,25 +1,25 @@
 package wikibooks.hadoop.chapter04;
 
-import java.io.IOException;
-import java.util.StringTokenizer;
-
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class WordCountMapper extends
-		Mapper<LongWritable, Text, Text, IntWritable> {
+        Mapper<LongWritable, Text, Text, IntWritable> {
 
-	private final static IntWritable one = new IntWritable(1);
-	private Text word = new Text();
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
 
-	public void map(LongWritable key, Text value, Context context)
-			throws IOException, InterruptedException {
-		StringTokenizer itr = new StringTokenizer(value.toString());
-		while (itr.hasMoreTokens()) {
-			word.set(itr.nextToken());
-			context.write(word, one);
-		}
-	}
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+        StringTokenizer itr = new StringTokenizer(value.toString().replace("”", " ").replace("“", " ").replace("?", " ").replace("\"", " ").replace(".", " ").replace(",", " "));
+        while (itr.hasMoreTokens()) {
+            word.set(itr.nextToken());
+            context.write(word, one);
+        }
+    }
 }
